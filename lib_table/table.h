4119b9ba51e45a859e9fbc8d4523af17d44a7939
RomanovMichail <vonamorm@mail.ru>
2025-02-26 20:10:08 +0300

    added realization table

--- lib_table/table.h
+++ lib_table/table.h
@@ -17,3 +17,38 @@ public:
     Tkey insert(Tval value);
 };
 
+template <class Tkey, class Tval>
+UnsortedTable<Tkey, Tval>::UnsortedTable(const Tlist<TPair<Tkey, Tval>>& list) {
+    _data = list;
+    _data = list.get_size();
+}
+
+template <class Tkey, class Tval>
+UnsortedTable<Tkey, Tval>::UnsortedTable(const UnsortedTable<Tkey, Tval>& table) {
+    _data = table._data;
+    _size = table._size;
+}
+
+template <class Tkey, class Tval>
+UnsortedTable<Tkey, Tval>& UnsortedTable<Tkey, Tval>::operator=(const UnsortedTable<Tkey, Tval>& table) {
+    if (table != (*this)) {
+        _data = table._data;
+        _size = table._size;
+    }
+    return *this;
+}
+
+template <class Tkey, class Tval>
+Tkey UnsortedTable<Tkey, Tval>::insert(Tval value) {
+    Tkey new_key = generate_unique_key(); 
+    TPair<Tkey, Tval> new_pair(new_key, value); 
+    _data.add(new_pair);
+    _size++; 
+    return new_key; 
+}
+
+template <class Tkey, class Tval>
+Tkey generate_unique_key() {
+    static Tkey current_key = 0; 
+    return ++current_key;
+}
